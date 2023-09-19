9931d5b6f1335a093af96480c5abc02aad129288
Gribbl3 <allenjoseph.lozada.21@usjr.edu.ph>
2023-09-19 20:40:46 +0800

    Add project files.

---
+++ Replica_WinWin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Replica_WinWin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Replica_WinWin/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Replica_WinWin
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
