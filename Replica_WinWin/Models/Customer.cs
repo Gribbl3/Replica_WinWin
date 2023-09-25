a718974c8f9652a2f5a6b0456b1f10b8ec391c9e
Gribbl3 <allenjoseph.lozada.21@usjr.edu.ph>
2023-09-25 11:42:56 +0800

    Added: Customer Model.

---
+++ Replica_WinWin/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace Replica_WinWin.Models
+{
+    public class Customer : Person
+    {
+        public string CustomerId { get; set; }
+        public string GetCustomerDetails()
+        {
+            return $"Customer Id: {CustomerId} \n" +
+                $"Name: {GetFullName()} \n" +
+                $"Username: {Username} \n" +
+                $"Email Address: {EmailAddress} \n" +
+                $"Birthdate: {Birthdate} \n" +
+                $"Home Address: {HomeAddress} \n";
+        }
+    }
+}
+
+
