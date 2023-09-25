6e22792e888dcc48d480a201a1f537b72ecf1926
Gribbl3 <allenjoseph.lozada.21@usjr.edu.ph>
2023-09-25 12:07:07 +0800

    Add a Mall location model class.

---
+++ Replica_WinWin/Models/MallLocation.cs
@@ -0,0 +1,16 @@
+﻿namespace Replica_WinWin.Models
+{
+    public class MallLocation
+    {
+        public string MallLocationId { get; set; }
+        public string MallLocationAddress { get; set; }
+
+        public override string ToString()
+        {
+            return $"Mall Location: {this.MallLocationAddress}";
+        }
+    }
+
+}
+
+
